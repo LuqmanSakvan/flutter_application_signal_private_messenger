1640583a6fa17a6a4aa133f72f68918b6216a8d1
Yousif <johnshelby121212@gmail.com>
2023-02-18 20:55:49 +0300

    Screen1

--- lib/YousifRaed/screen.dart
+++ lib/YousifRaed/screen.dart
@@ -1,3 +1,11 @@
-void main() {
- 
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Screen1 extends StatelessWidget {
+  const Screen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
