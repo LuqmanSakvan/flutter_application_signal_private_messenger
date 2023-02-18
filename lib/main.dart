8863f6de1a12177fbf5023610f66ebd7da67c7d7
Yousif <johnshelby121212@gmail.com>
2023-02-18 20:57:10 +0300

    Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,24 @@
-void main(){
+import 'package:flutter/material.dart';
+import 'package:signal_private_messenger/YousifRaed/screen.dart';
 
-  
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Card',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Screen1() ,
+    );
+  }
 }
