a2033a4b080006971de655bed7507e1f70bcb0ce
LuqmanSakvan <luqmansakvan@gmail.com>
2023-03-02 19:56:19 +0300

    insert ChatMessage Class into models file

--- lib/Luqman/Models.dart
+++ lib/Luqman/Models.dart
@@ -9,3 +9,9 @@ class ChatUsers {
   ChatUsers(
       {required this.name, required this.messageText, required this.time});
 }
+
+class ChatMessage {
+  String messageContent;
+  String messageType;
+  ChatMessage({required this.messageContent, required this.messageType});
+}
