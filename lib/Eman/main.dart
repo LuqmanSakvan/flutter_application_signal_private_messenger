e70906d11bdfdff89b5146765e6c73e532de0f13
EmanEhalid <ek320125@gmail.com>
2023-03-14 22:07:22 +0300

    call SearchScreen in main file

--- lib/Eman/main.dart
+++ lib/Eman/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:signal_private_messenger/Eman/Search.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      //home:
+      home:SearchScreen(),
     );
   }
 }
