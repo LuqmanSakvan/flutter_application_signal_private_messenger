72b05556cea92b81a5cab7d92032ad3c75b648e6
LuqmanSakvan <luqmansakvan@gmail.com>
2023-03-18 22:24:27 +0300

    main axis align to center

--- lib/Luqman/Navigation_bar_buttom_bar.dart
+++ lib/Luqman/Navigation_bar_buttom_bar.dart
@@ -45,6 +45,7 @@ class _HomeState extends State<Home> {
                       });
                     },
                     child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [],
                     ),
                   )
