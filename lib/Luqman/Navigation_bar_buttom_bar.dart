a171f0159b1db2cba5e3fbbac132e8a9d95b223a
LuqmanSakvan <luqmansakvan@gmail.com>
2023-03-18 22:21:50 +0300

    assign currentTap to 0;

--- lib/Luqman/Navigation_bar_buttom_bar.dart
+++ lib/Luqman/Navigation_bar_buttom_bar.dart
@@ -41,6 +41,7 @@ class _HomeState extends State<Home> {
                     onPressed: () {
                       setState(() {
                         currentScreen = Myhome();
+                        currentTab = 0;
                       });
                     },
                   )
