49512c8a086ca90349b630f82aeb90ae2a4082f8
LuqmanSakvan <luqmansakvan@gmail.com>
2023-03-18 22:35:57 +0300

    main axis to center for column

--- lib/Luqman/Navigation_bar_buttom_bar.dart
+++ lib/Luqman/Navigation_bar_buttom_bar.dart
@@ -70,6 +70,7 @@ class _HomeState extends State<Home> {
                       });
                     },
                     child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [],
                     ),
                   ),
