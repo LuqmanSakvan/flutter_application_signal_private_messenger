f9b33dec93d042c6c5b8546cab5be64f5db1675b
LuqmanSakvan <luqmansakvan@gmail.com>
2023-03-18 21:59:09 +0300

    add pageStorage inside scaffold

--- lib/Luqman/Navigation_bar_buttom_bar.dart
+++ lib/Luqman/Navigation_bar_buttom_bar.dart
@@ -21,6 +21,10 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: PageStorage(
+        child: currentScreen,
+      ),
+    );
   }
 }
