22a19d0fbb85dec5eb47d0a0aaaf9034b623b126
LuqmanSakvan <luqmansakvan@gmail.com>
2023-03-18 22:02:50 +0300

    add row inside container

--- lib/Luqman/Navigation_bar_buttom_bar.dart
+++ lib/Luqman/Navigation_bar_buttom_bar.dart
@@ -29,6 +29,7 @@ class _HomeState extends State<Home> {
       bottomNavigationBar: BottomAppBar(
         child: Container(
           height: 60,
+          child: Row(),
         ),
       ),
     );
