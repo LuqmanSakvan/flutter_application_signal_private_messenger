ae4e4177d97ab8e3993b080e777e5286afffaba4
LuqmanSakvan <luqmansakvan@gmail.com>
2023-03-22 00:18:34 +0300

    Add children

--- lib/Luqman/Home_Screen.dart
+++ lib/Luqman/Home_Screen.dart
@@ -71,7 +71,9 @@ class Myhome extends StatelessWidget {
                 ]),
               ),
               PopupMenuItem(
-                child: Row(),
+                child: Row(
+                  children: [],
+                ),
               )
             ],
           ),
