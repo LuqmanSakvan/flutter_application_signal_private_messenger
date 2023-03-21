e8e532c927d85c71729081e13eef71569e0cf63a
EmanEhalid <ek320125@gmail.com>
2023-03-21 22:47:27 +0300

    using subtitle

--- lib/Eman/List.dart
+++ lib/Eman/List.dart
@@ -20,6 +20,7 @@ class ListUI extends StatelessWidget {
       itemBuilder: (context, index) {
         return ListTile(
           title: Text(myuserlist[index].name),
+          subtitle: Text(myuserlist[index].sub_name),
         );
       }),
     );
