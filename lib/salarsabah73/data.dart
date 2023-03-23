df34fc540ddb4502ffd181d8556eadbffbf4be35
salarwais <salar.wais01@gmail.com>
2023-03-23 18:07:18 +0300

    addmodels

--- lib/salarsabah73/data.dart
+++ lib/salarsabah73/data.dart
@@ -7,3 +7,13 @@ class user{
     this.phone
   });
 }
+
+List<user> userlist = [
+  user(
+  name: "salar sabah",
+  phone: "+964 750 555 2244"),
+  user(
+  name: "ahmed salah",
+  phone: "+964 750 152 3222"),
+
+];
