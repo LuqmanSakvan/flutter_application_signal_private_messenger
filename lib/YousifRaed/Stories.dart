8a096b28421710764a70de9005a8040c3e2cf140
Yousif <johnshelby121212@gmail.com>
2023-03-23 17:06:38 +0300

    simple text widget for "Chats"

--- lib/YousifRaed/Stories.dart
+++ lib/YousifRaed/Stories.dart
@@ -24,6 +24,7 @@ body: Stack(
     Positioned(top:655,child: Container(height: 150,width: 400,color: Colors.black12,)),
     Positioned(top:670,left:80,child: ElevatedButton(onPressed: (() {}), child:Icon(Icons.message_rounded),style: ElevatedButton.styleFrom(shape: RoundedRectangleBorder(),backgroundColor: Colors.transparent), )),
     Positioned(top:670,left:270,child: ElevatedButton(onPressed:(() {}), child: Icon(Icons.amp_stories_outlined),style: ElevatedButton.styleFrom(shape:RoundedRectangleBorder(),backgroundColor: Colors.black12),)),
+    Positioned(top:720,left:96,child: Text("Chats",style: TextStyle(fontWeight: FontWeight.w500),)),
     
 
 
