2355a32d7606e7ef184dbdc0284f0a13b118d49e
EmanEhalid <ek320125@gmail.com>
2023-03-23 17:15:03 +0300

    update datamodel

--- lib/Eman/DataModel.dart
+++ lib/Eman/DataModel.dart
@@ -1,20 +1,25 @@
 class User_D_M{
 String name;
+String name2;
+String? sub_name2;
 String? sub_name;
 
 User_D_M({
   required this.name,
-    required this.sub_name
+    required this.sub_name,
+    required this.name2,
+    required this.sub_name2
+
 });
 }
 
 List<User_D_M>myuserlist=[
-  User_D_M(name:"Theme" , sub_name:"System defult"),
+  User_D_M(name:"Theme" , sub_name:"System defult",name2: "",sub_name2: ""),
 
-  User_D_M(name:"Chat color & wallpaper",sub_name: ""),
+  User_D_M(name:"Chat color & wallpaper",sub_name: "",name2: "",sub_name2: ""),
 
-  User_D_M(name:"Message font size" , sub_name:"Normal"),
+  User_D_M(name:"Message font size" , sub_name:"Normal",name2: "",sub_name2: ""),
 
-  User_D_M(name:"Language" , sub_name:"System defult"),
+  User_D_M(name:"Language" , sub_name:"System defult",name2: "",sub_name2: ""),
   
 ];
