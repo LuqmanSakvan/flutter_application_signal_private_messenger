10f9dd5a8298cc0fbd3e1af79f7e0b819dd95a9d
salarwais <salar.wais01@gmail.com>
2023-03-25 14:11:29 +0300

    sizedbox for data

--- lib/salarsabah73/profile.dart
+++ lib/salarsabah73/profile.dart
@@ -108,6 +108,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                         fontSize: 20,
                       ),
                     ),
+                    const SizedBox()
                   ],
                 ),
               ),
